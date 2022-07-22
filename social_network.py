5c6bf6d6ef0202887c84f1da7dac04f2b2e9da3c
Benedict Quartey <benedict_quartey@brown.edu>
2022-07-22 17:38:16 -0400

    fixed changes

--- social_network.py
+++ social_network.py
@@ -4,23 +4,6 @@ import social_network_ui
 
 
 
-def mainMenu():
-    print("")
-    print("1. Create a new account")
-    print("2. Manage my account")
-    print("3. Quit")
-    print("********************************************************")
-    return input("Please Choose a number: ")
-
-def manageAccountMenu():
-    print("1. Edit my details")
-    print("2. Add a friend")
-    print("3. View all my friends")
-    print("4. View all my messages")
-    print("5. <- Go back ")
-    return input("Please Choose a number: ")
-
-
 #Create instance of main social network object
 ai_social_network = SocialNetwork()
 
