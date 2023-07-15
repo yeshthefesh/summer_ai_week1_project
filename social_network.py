4f677791953326ecad68f25e1a7c58a93ca50d62
Joshua Yan <joshua.j.yan@gmail.com>
2023-07-15 19:17:24 -0400

    added social network

--- social_network.py
+++ social_network.py
@@ -14,6 +14,7 @@ if __name__ == "__main__":
     print("########################################################")
     last_menu = None
     choice = social_network_ui.mainMenu()
+    accessed = False
 
     while True: 
         if choice == "1":
@@ -24,13 +25,50 @@ if __name__ == "__main__":
             inner_menu_choice = social_network_ui.manageAccountMenu()
             #Handle inner menu here
             while True:
-                if inner_menu_choice == "5":
+                if inner_menu_choice == "6":
                     break
                 else:
                     inner_menu_choice = social_network_ui.manageAccountMenu()
+                    if inner_menu_choice == "1":
+                        if not accessed:
+                            print("You have to access an account first.")
+                            break
+                        print("Your current account details:")
+                        print("Name: ",current_account.id)
+                        print("Age: ",current_account.year)
+                        current_account.id = input("New name: ")
+                        current_account.year = input("New age: ")
+                    elif inner_menu_choice == "2":
+                        if not accessed:
+                            print("You have to access an account first.")
+                            break
+                        current_account.add_friend(ai_social_network)
+                    elif inner_menu_choice == "3":
+                        if not accessed:
+                            print("You have to access an account first.")
+                            break
+                        print("Your friends are:")
+                        for friend in current_account.friendlist:
+                            print(friend.id)
+                    elif inner_menu_choice == "4":
+                        if not accessed:
+                            print("You have to access an account first.")
+                            break
+                        current_account.send_message(current_account)
+                    elif inner_menu_choice == "5":
+                        if not accessed:
+                            print("You have to access an account first.")
+                            break
+                        print("Your messages:")
+                        for message in current_account.messages:
+                            print(message)
 
         elif choice == "3":
-            print("Thank you for visiting. Goodbye3")
+            current_account = ai_social_network.access_account()
+            accessed = True
+
+        elif choice == "4":
+            print("Thank you for visiting. Goodbye.")
             break
 
         else:
