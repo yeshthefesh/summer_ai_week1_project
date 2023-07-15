28312b0aabb3f1fce610c4e81c5f541431665d1a
Joshua Yan <joshua.j.yan@gmail.com>
2023-07-15 19:16:30 -0400

    added social network classes

--- social_network_classes.py
+++ social_network_classes.py
@@ -20,19 +20,51 @@ class SocialNetwork:
     def  create_account(self):
         #implement function that creates account here
         print("Creating ...")
-        pass
+        name = input("What is your name?\n")
+        age = input("How old are you?\n")
+        self.list_of_people.append(Person(name,age))
 
+    def access_account(self):
+        if len(self.list_of_people) > 0:
+            print("The accounts that you can access are:")
+            for i,account in enumerate(self.list_of_people):
+                print(i+1,account.id)
+            choice = input("Which account would you like to access? (choose a number): ")
+            if int(choice)-1 in range(len(self.list_of_people)):
+                current_account = self.list_of_people[int(choice)-1]
+                print("You are now accessing",str(current_account.id))
+            else:
+                print("The account you have chosen is not one of the ones listed.")
+        else:
+            print("You have not created any accounts yet.")
+        return current_account
 
 class Person:
     def __init__(self, name, age):
         self.id = name
         self.year = age
         self.friendlist = []
+        self.messages = []
 
-    def add_friend(self, person_object):
-        #implement adding friend. Hint add to self.friendlist
-        pass
+    def add_friend(self,network):
+        print("List of users:")
+        for i,user in enumerate(network.list_of_people):
+            print(i+1,user.id)
+        chosen = input("Who would you like to add?\n")
+        new_friend = network.list_of_people[int(chosen)-1]
+        if new_friend in self.friendlist:
+            print("That person is already your friend.")
+        elif new_friend == self:
+            print("You cannot add yourself.")
+        else:
+            self.friendlist.append(new_friend)
+            print(f"You have added {new_friend.id}.")
 
-    def send_message(self):
-        #implement sending message to friend here
-        pass
+    def send_message(self,current_account):
+        print("List of friends:")
+        for i,friend in enumerate(self.friendlist):
+            print(i+1,friend.id)
+        chosen = input("Who would you like to send a message to?\n")
+        message = input("What is your message?\n")
+        self.friendlist[int(chosen)-1].messages.append(current_account.id+": "+message)
+        print("You have sent a message to",self.friendlist[int(chosen)-1].id)
